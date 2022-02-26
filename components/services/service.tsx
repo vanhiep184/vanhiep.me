9978b76984320f53233ffedc001212153cd8e530
vanhiep184 <levanhiep.95.95@gmail.com>
2022-02-26 19:56:24 +0700

    add quotes

--- components/services/service.tsx
+++ components/services/service.tsx
@@ -1,14 +1,24 @@
+import styles from "./service.module.css";
 export default function Service() {
   return (
-    <section id="services" className="first-page">
-      <div className="hidden md:block">
-        <h2>Design work</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <section id="services" className="">
+      <div className={styles.quoteContainer}>
+        <h3 className="text-xl lg:text-4xl md:text-2xl">
+          Any fool can write code that a computer can understand.Good
+          programmers write code that humans can understand.
+        </h3>
+        <p>- Martin Fowler</p>
       </div>
-      <img
-        src="https://images.unsplash.com/photo-1636493532482-44ec2354476b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80"
-        alt="Design work"
-      />
+      {/* <div className="first-page">
+        <div className="hidden md:block">
+          <h4>Design work</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <img
+          src="https://images.unsplash.com/photo-1636493532482-44ec2354476b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80"
+          alt="Design work"
+        />
+      </div> */}
     </section>
   );
 }
